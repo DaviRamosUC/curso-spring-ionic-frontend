5af726d9aecb0b774f3253217692b3c81d89ba97
Davi <davi.lima@ucsal.edu.br>
2021-05-31 09:31:56 -0300

    Criando um formGroup para controlar o funcionario

--- src/services/cliente.service.ts
+++ src/services/cliente.service.ts
@@ -2,7 +2,7 @@ import { StorageService } from './storage.service';
 import { API_CONFIG } from './../config/api.config';
 import { Observable } from 'rxjs/Rx';
 import { clienteDTO } from './../models/cliente.dto';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -20,6 +20,6 @@ export class ClienteService {
 
     getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType: 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
     }
 }
