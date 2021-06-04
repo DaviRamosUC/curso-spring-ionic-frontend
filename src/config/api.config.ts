cb8e74904d9759bec0669c0f161971e6ebb2e69b
Davi <davi.lima@ucsal.edu.br>
2021-06-04 07:32:45 -0300

    Deploy

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,6 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    //https://curso-spring-ionic-uc.herokuapp.com
+    //http://localhost:8080
+    baseUrl: "https://curso-spring-ionic-uc.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-uc.s3-sa-east-1.amazonaws.com"
 }
